477561a3de60eadc83e28be0ac9e5d081fb70a74
Aylin Yay <aylinyay@outlook.com>
2023-03-24 17:48:58 +0300

    Add project files.

---
+++ Algoritma_Sorulari/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_Sorulari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SORU 1
+            //Console.WriteLine("Lütfen pozitif bir sayı giriniz : ");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Adet Sayısı giriniz : ");
+            //int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = sayi; i < sayi2; i++)
+            //{
+
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //SORU 2
+            //Console.WriteLine("Please enter 2 positive numbers");
+            //Console.Write("First number: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Second number: ");
+            //int m = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("You need to enter {0} positive numbers.", n);
+
+            //int[] numberarray = new int[n];
+
+            //for (int i = 1; i < n + 1; i++)
+            //{
+
+            //    Console.Write("Number {0}: ", i);
+            //    numberarray[i - 1] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //foreach (var number in numberarray)
+            //{
+            //    if (number == m)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //    else if (number % m == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //SORU 3
+            //Console.Write("Pozitif Bir Sayi Girin :");
+            //int sayi = Convert.ToInt32(Console.ReadLine());
+
+            //if (sayi<=0)
+            //{
+            //    Console.Write("Lütfen Pozitif Bir Sayi Girin :");
+            //    sayi = Convert.ToInt32(Console.ReadLine());
+
+            //}
+
+
+            //string[] kelime = new string[sayi];
+            //for (int i = 1; i < sayi; i++)
+            //{
+            //    Console.Write(i + ". Kelimeyi Giriniz : ");
+            //    string sayi2 = Console.ReadLine();
+            //    kelime[i] = Convert.ToString(sayi2);
+            //}
+
+            //Array.Reverse(kelime);
+            //foreach (var item in kelime)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.ReadLine();
+
+
+            //SORU 4
+            //Console.WriteLine("Cümle yazınız.");
+            //string input = Console.ReadLine();
+            //string[] words = input.Split(' ');
+            //int w = words.Count();
+            //char[] c = input.Replace(" ", string.Empty).ToCharArray();
+            //int c1 = c.Count();
+            //Console.WriteLine("Kelime sayısı: {0}, Harf sayısı: {1}", w, c1);
+            //Console.ReadKey();
+        }
+    }
+}
